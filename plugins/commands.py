6647f5e29d1fa59cad14fd764fdf697a7be38dca
David Serrano <david.nonamedguy@gmail.com>
2011-11-08 13:07:38 +0000

    Command permission handled differently.

--- plugins/commands.py
+++ plugins/commands.py
@@ -12,51 +12,37 @@ def get_conf():
     f.close()
     return conf
 
-def has_access(bot, user, permission, channel, masters):
-    # pure copy pasta from the last version, ihih
-    # it is ugly, I know...
-    
-    perm_map={'all': '*', 'voice': '+', 
-                'op': '@', 'master': 'M'}
-    perm = perm_map[permission]
-    
-    user_perm = '*'
-    # firstly and foremost: can i run this?
-    # FIXME: If the message is private, how should bot check for permissions?
-    if user.get_nick() in masters:
-        user_perm = 'M'
-    elif channel != None:
-        user_perm = bot.get_users(self,channel)[user.get_nick()].get_status()
-    
-    return not(not (user.get_nick() in masters and user_perm == 'M')) \
-        and (not (perm == '@' and user_perm == '@')) \
-        and (not (perm == '+' and ((user_perm == '@') 
-                                         or (user_perm == '+') 
-                                         or (user_perm == 'M')))) \
-        and (not (perm == '*'))
 
 # Lower permissions have higher precedence
-def check_permission(bot, command, user, channel):
-    conf = get_conf()
-    perms = conf['permissions']
-    masters = conf['masters']
-    
-    if command in perms['all']:
-        return True
-    elif command in perms['voice']:
-        return has_access(bot, user, 'voice', channel, masters)
-    elif command in perms['op']:
-        return has_access(bot, user, 'op', channel, masters)
-    elif command in perms['master']:
-        return has_access(bot, user, 'master', channel, masters)
-    
-    return True
-
 class CommandsPlugin(PluginBase):
     
     def plugin_init(self):
         self.conf = get_conf()
         self.register_event("channel_msg", self.on_channel_msg)
+
+    def check_permission(self, command, user, channel):
+        """
+        Checks if a command can be run by a certain user on a certain channel
+        """
+        perm_map = {"voice": "+", "op": "@", "all" : ""}
+        
+        # If the command is not set in the permissions, it is assumed
+        # that the command can be used by anyone
+        if command in self.conf['permissions']: 
+            cmd_perm = [command]
+        else:
+            return True
+        
+        # Now with the permission check
+        user_status = self.bot.get_user_status(channel, user.get_nick())
+        cmd_status = perm_map[cmd_perm]
+        
+        return (cmd_perm == "all" or (user.get_nick() in self.conf['masters'])) \
+            or (user_status == '@' and
+                (cmd_status == '@' or cmd_status == '+' or cmd_status == '')) \
+            or (user_status == '+' and
+                (cmd_status == '+' or cmd_status == '')) \
+            or (user_status == cmd_status)
         
     def on_channel_msg(self, user, channel, msg):
         # FIXME: Sometime this stops working on the presence of strange stuff
@@ -69,7 +55,7 @@ class CommandsPlugin(PluginBase):
             tokens = cmd.split(" ")
             cmd = tokens[0]
             
-            if not(check_permission(self.bot, cmd, user, channel)):
+            if not(self.check_permission(cmd, user, channel)):
                 return
             
             if (len(tokens) > 1):
