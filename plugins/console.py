c866a09c9b92345c8b5fb17db1c272c583b91f5c
David Serrano <david.nonamedguy@gmail.com>
2011-11-08 12:09:55 +0000

    More events handled in the console plugin.

--- plugins/console.py
+++ plugins/console.py
@@ -12,14 +12,18 @@ class ConsolePlugin(PluginBase):
         while True:
             sys.stdout.write("> ")
             line = sys.stdin.readline()
-            
             if line == "": return
             line = line[:-2]
+            
+            self.parse(line)
+    
+    def parse(self, line):
+        if line == "": return
         
-            if line.startswith("/"):
-                line = line[1:]
-                self.bot.send(line)
-        
+        if line.startswith("/"):
+            line = line[1:]
+            self.bot.send(line)
+    
     def plugin_init(self):
         # TODO: Process more events
         self.register_event("connect", self.run_it)
@@ -28,23 +32,32 @@ class ConsolePlugin(PluginBase):
         
         self.register_event("join", self.on_join)
         self.register_event("part", self.on_part)
+        self.register_event("quit", self.on_quit)
         
         self.register_event("numeric", self.on_numeric)
+        
+        self.register_event("cmd_raw", self.on_raw_cmd)
         
     def run_it(self):
         FunThread(self.work, []).start()
     
+    def on_raw_cmd(self, user, channel, cmd, args):
+        self.parse(" ".join(args))
+
     def on_join(self, user, channel):
         print "* %s joined %s" % (user, channel)
     
     def on_part(self, user, channel, msg):
         print "* %s left %s (%s)" % (user, channel, msg)
         
+    def on_quit(self, user, reason):
+        print "* %s has quit (%s)" % (user, reason)
+        
     def on_channel_msg(self, user, channel, msg):
-        print "<%s@%s> - %s" % (user, channel, msg)
+        print "<%s@%s> %s" % (user, channel, msg)
         
     def on_priv_msg(self, user, msg):
-        print "<%s> - %s" % (user, msg)
+        print "<%s> %s" % (user, msg)
 
     def on_numeric(self, *tokens):
         sys.stdout.write("* ")
