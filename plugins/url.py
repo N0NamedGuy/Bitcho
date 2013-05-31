daa9dd5e15ea64aeb4613fc3e6680334f5b2d719
David <david.nonamedguy@gmail.com>
2013-05-31 17:32:02 +0100

    Removed unrelated comment

--- plugins/url.py
+++ plugins/url.py
@@ -11,7 +11,6 @@ from plugin_base import PluginBase
 
 url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
 
-# Lower permissions have higher precedence
 class UrlPlugin(PluginBase):
     
     def parse_url(self, url, dest):
